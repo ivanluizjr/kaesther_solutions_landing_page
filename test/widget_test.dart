19343c2cff74d0df0d5c4dffde75590422a54c47
ivanluizjr <ivanluizjr@hotmail.com>
2026-03-07 20:55:24 -0300

    Fazendo correcoes

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:kaesther_solutions_landing_page/app.dart';
-
-void main() {
-  testWidgets('Landing page renders', (WidgetTester tester) async {
-    await tester.pumpWidget(const KaestherApp());
-    expect(find.text('Soluções Digitais'), findsOneWidget);
-  });
-}
